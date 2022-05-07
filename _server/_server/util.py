a9607442ce0895b6a44514a653664e65658917de
Ayat <alkayedayat93@gmail.com>
2022-05-07 07:09:22 +0300

    error when i use the saved model in the server

---
+++ _server/_server/util.py
@@ -0,0 +1,32 @@
+import pickle
+import json
+import joblib
+
+_location = None
+_data_columns = None
+_model = None
+
+
+def load_saved_files():
+    global _location , _data_columns , _model
+    print('i saved them')
+
+    with open('./_server/columns.json' , 'r') as f :
+       _data_columns = json.load(f)['data_columns']
+       _location = _data_columns[3:]
+
+    # mj = joblib.load('model_joblib')
+
+
+    # with open('./_server/func_pickle.pickle' , 'rb') as f:
+    #     _model = pickle.load(f)
+
+def get_location_names():
+    return _location
+
+
+if __name__ == '__main__':
+    load_saved_files()
+    print(get_location_names())
+    
+    
